7de1813a50de0d447e5f3e764c64d4d9fc44718a
Arthur <arthurbarbosa93@gmail.com>
2022-02-04 17:15:44 -0300

    tentando requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${ItemID}`;
+    const promise = await fetch(url);
+    
+    const response = await promise.json();
+    return response;  
+  } catch (error) { 
+    return (error);
+  }
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));  
 
   return section;
 }
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -37,16 +37,33 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  return li;
+  return li;  
 }
+ /* requisito 2 pegando dados da api fetchitem para usar no botão e adicionar ao carrinho */ 
+const carrinhoProdutos = async (e) => {
+  const item = e.target.parentNode;
+  const items = getSkuFromProductItem(item);
+  const { id, title, price } = await fetchItem(items);
+  const classe = document.querySelector('.cart__items');
+  const carrinho = createCartItemElement({ sku: id, name: title, salePrice: price });
+  classe.appendChild(carrinho);
+};
 
- /* onloads com fetchproducts com argumento computador, then no results do API, foreach para encontrar elementos da const id, title, thumbnail, guardando o element usamos no creator products passando o retorno. 
- selecionando a classe, criamos um filho com retorno de items, criando assim a secção de itens no carregamento da página */
-window.onload = () => {
-  fetchProducts('computador').then(({ results }) => results.forEach((element) => {
+const pesquisaProdutos = async (mercadorias) => {
+  const array = await fetchProducts(mercadorias);
+  array.results.forEach((element) => {
     const { id, title, thumbnail } = element;
     const item = document.querySelector('.items');
     const items = createProductItemElement({ sku: id, name: title, image: thumbnail });
     item.appendChild(items);
-  }));
+  });
+};
+ /* onloads com fetchproducts com argumento computador, then no results do API, foreach para encontrar elementos da const id, title, thumbnail, guardando o element usamos no creator products passando o retorno. 
+ selecionando a classe, criamos um filho com retorno de items, criando assim a secção de itens no carregamento da página *
+ vamos colocar essa fetchprodutcs numa const pesquisaProdutos fora do onload e chamar no onload como método assincrono asyns/wait para melhorar a manipulação de dados 
+ tive essa lógica vendo a mentoria - pegando a classe item add e percorrendo com foreach e colocando evento de click no carrinhoProdutos */
+ window.onload = async () => {
+  await pesquisaProdutos('computador');
+  const botaoCarrinho = document.querySelectorAll('.item__add');
+  botaoCarrinho.forEach((element) => element.addEventListener('click', carrinhoProdutos));
  };
