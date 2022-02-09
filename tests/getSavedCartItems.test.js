8caec0916f5c4aa762142504104390ab14c769a1
Arthur <arthurbarbosa93@gmail.com>
2022-02-09 17:55:58 -0300

    testando 10 e 11

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,5 +1,13 @@
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui  
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  });
 });
