cb80373c6e888cfc07592229fc44c4c9751d3b66
Arthur <arthurbarbosa93@gmail.com>
2022-02-10 15:40:50 -0300

    testando 11

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,3 +1,6 @@
+const localStorageSimulator = require('../mocks/localStorageSimulator');
+const getSavedCartItems = require('../helpers/getSavedCartItems');
+
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
